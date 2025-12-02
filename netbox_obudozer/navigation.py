6482d19bea54e4e0d414ad22c3526b117da22d1d
Braminn <svalip01@gmail.com>
2025-12-02 22:42:25 +0300

    перенос ссылки в меню с ВМ

--- netbox_obudozer/navigation.py
+++ netbox_obudozer/navigation.py
@@ -2,52 +2,45 @@
 Навигация плагина netbox_obudozer
 
 Определяет структуру меню плагина в интерфейсе NetBox.
-Использует группированное меню (PluginMenu) для лучшей организации.
+Добавляет пункты в существующее меню Virtualization.
 """
-from netbox.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
+from netbox.plugins import PluginMenuButton, PluginMenuItem
 from netbox.choices import ButtonColorChoices
 
 
-menu = PluginMenu(
-    label='Obudozer',
-    groups=(
-        ('vCenter', (
-            PluginMenuItem(
+# Добавляем пункты в раздел Virtualization
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_obudozer:sync_vcenter',
+        link_text='vCenter Sync',
+        permissions=['virtualization.add_virtualmachine'],
+        buttons=(
+            PluginMenuButton(
                 link='plugins:netbox_obudozer:sync_vcenter',
-                link_text='vCenter Sync',
-                permissions=['virtualization.add_virtualmachine'],
-                buttons=(
-                    PluginMenuButton(
-                        link='plugins:netbox_obudozer:sync_vcenter',
-                        title='Sync Now',
-                        icon_class='mdi mdi-sync',
-                        color=ButtonColorChoices.BLUE,
-                        permissions=['virtualization.add_virtualmachine']
-                    ),
-                )
+                title='Sync Now',
+                icon_class='mdi mdi-sync',
+                color=ButtonColorChoices.BLUE,
+                permissions=['virtualization.add_virtualmachine']
             ),
-        )),
-        ('Бизнес-сервисы', (
-            PluginMenuItem(
-                link='plugins:netbox_obudozer:businessservice_list',
-                link_text='Сервисы',
-                permissions=['netbox_obudozer.view_businessservice'],
-                buttons=(
-                    PluginMenuButton(
-                        link='plugins:netbox_obudozer:businessservice_add',
-                        title='Добавить',
-                        icon_class='mdi mdi-plus-thick',
-                        color=ButtonColorChoices.GREEN,
-                        permissions=['netbox_obudozer.add_businessservice']
-                    ),
-                )
-            ),
-            PluginMenuItem(
-                link='plugins:netbox_obudozer:servicevmassignment_list',
-                link_text='Привязки VM',
-                permissions=['netbox_obudozer.view_servicevmassignment'],
-            ),
-        )),
+        )
     ),
-    icon_class='mdi mdi-cloud'
+    PluginMenuItem(
+        link='plugins:netbox_obudozer:businessservice_list',
+        link_text='Бизнес-сервисы',
+        permissions=['netbox_obudozer.view_businessservice'],
+        buttons=(
+            PluginMenuButton(
+                link='plugins:netbox_obudozer:businessservice_add',
+                title='Добавить',
+                icon_class='mdi mdi-plus-thick',
+                color=ButtonColorChoices.GREEN,
+                permissions=['netbox_obudozer.add_businessservice']
+            ),
+        )
+    ),
+    PluginMenuItem(
+        link='plugins:netbox_obudozer:servicevmassignment_list',
+        link_text='Привязки VM',
+        permissions=['netbox_obudozer.view_servicevmassignment'],
+    ),
 )
